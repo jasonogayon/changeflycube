43ddd758289189d5ef04e7db01df4d45a1d2bf00
Jason Ogayon <jason.ogayon@directwithhotels.com>
2018-09-24 14:42:39 +0800

    Added simple animation to changefly logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
 
 void main() => runApp(ChangeFly());
 
@@ -21,21 +22,35 @@ class Cube extends StatefulWidget {
   _CubeState createState() => _CubeState();
 }
 
-class _CubeState extends State<Cube> {
+class _CubeState extends State<Cube> with SingleTickerProviderStateMixin {
+  Animation animation;
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      duration: Duration(milliseconds: 5000),
+      vsync: this
+    );
+    animation = Tween(begin: 0.0, end: 120.0).animate(animationController);
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        animationController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        animationController.fling();
+      }
+    });
+    animationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
         children: <Widget>[
-          Center(
-            child: Container(
-              padding: EdgeInsets.only(top: 100.0),
-              child: Image.asset(
-                'assets/changefly-cube.png',
-                height: 200.0,
-              ),
-            ),
-          ),
+          Padding(padding: EdgeInsets.only(bottom: 200.0)),
+          LogoAnimation(animation: animation),
           Center(
             child: Container(
               padding: EdgeInsets.only(top: 20.0),
@@ -46,6 +61,29 @@ class _CubeState extends State<Cube> {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+}
+
+class LogoAnimation extends AnimatedWidget {
+  LogoAnimation({Key key, Animation animation})
+    : super(key: key, listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    Animation animation = listenable;
+    return Center(
+      child: Container(
+        height: animation.value,
+        width: animation.value,
+        child: Image.asset('assets/changefly-cube.png'),
       ),
     );
   }
